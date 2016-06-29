af8bbc21fcdc1ea38c26438d64ac5af4eb86b394
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-06-29 16:07:22 +0700

    Add sample bash script

---
+++ osube.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+osube_script="/home/csaratakij/@Git_Repository/Me/Osube/osube.py"		# The path of osube.py
+extract_folder="/home/csaratakij/Opsu/Songs"						    # The path of extract folder (Osu!'s Songs folder)
+python3 $osube_script $extract_folder								    # Command (python3 osube.py [extract directory]
