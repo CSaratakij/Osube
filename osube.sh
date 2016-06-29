098e253476a2af40818153ea374e6c38953ea479
Chatchai Saratakij <Chatchai.qrz330@gmail.com>
2016-06-29 16:16:08 +0700

    Improve comment in sample bash

--- osube.sh
+++ osube.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-osube_script="/home/csaratakij/@Git_Repository/Me/Osube/osube.py"		# The path of osube.py
-extract_folder="/home/csaratakij/Opsu/Songs"						    # The path of extract folder (Osu!'s Songs folder)
-python3 $osube_script $extract_folder								    # Command (python3 osube.py [extract directory]
+
+# The path of osube.py
+osube_script="/home/csaratakij/@Git_Repository/Me/Osube/osube.py"
+
+# The path of extract folder (Osu!'s Songs folder)
+extract_folder="/home/csaratakij/Opsu/Songs"
+
+# Command (python3 osube.py [extract directory]
+python3 $osube_script $extract_folder
