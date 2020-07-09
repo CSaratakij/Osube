70a56c02391f8aa301668396b3d17f0dde86ffcf
Chatchai Saratakij <CSaratakij@gmail.com>
2020-07-10 06:54:00 +0700

    Fix : use argparse as a command line argument parser

--- README.md
+++ README.md
@@ -1,45 +1,13 @@
 # Osube
 Extract multiple [Osu!](https://osu.ppy.sh/)'s beatmaps (.osz)
 
-# Note
-- This is a command line program.
+# Dependencies
+- python3
 
-# Getting Start
-You can use program in these ways.
-(just my suggest.)
-(pick the one you like. ^^)
-
-1. Copy osube.py in your common beatmap's download folder and run this program in terminal with python.
-2. Create the bash file with specify extract folder, place that script to your common download folder and chmod +x that script ([See this example](osube.sh))
-3. Set this program in your environment. In case of linux, edit your /etc/environment ( edit with sudo :) )
-
-# To Use Program
-####Command :
-- Extract without keeping beatmap archives (.osz)
-```
-python osube.py [extract directory]
-```
-- Extract and keep beatmap archives (.osz)
-```
-python osube.py [extract directory] -k
-```
-1. Run this program in the folder that have osu!'s beatmaps file (.osz)
-2. Make sure you specify the extract folder. (It's a 2nd command line argument)
-Replace "extract directory" to your Songs folder in your osu! directory.
-3. Wait for the extraction to complete.
-4. Have fun with Osu! ( Great game by the way :) )
-
-# FAQ
-- Q: Why create this?
-- A : Well, The extraction achrive program in my OS (Lubuntu) doesn't create the folder after you extracted the file. Osu! Beatmaps need to make a one parent folder before extract the beatmaps and extracting the multiple beatmaps by hand is painful- -.
-
-- Q: But Osu! doesn't support linux natively, Are you using [Wine](https://www.winehq.org/)?
-- A : Wine is awesome but I prefer not to use it anymore. There's a alternative Osu! Client in desktop, It's call [Opsu!](https://itdelatrisu.github.io/opsu/).
-
-# Todo List
-- [x] Extract multiple beatmaps.
-- [x] Auto remove beatmaps when successful extracted.
-- [x] Add optional to keep beatmaps after extraction.
+# Getting Started
+1. Clone this repository
+2. Make osube.py executable
 
 #License
 - [MIT](LICENSE)
+
--- osube.py
+++ osube.py
@@ -4,152 +4,74 @@ import os
 import sys
 import zipfile
 import re
+import argparse
 
 from zipfile import ZipFile
 
+BEATMAP_PATTERN = "(\w.+osz)" 
 
-def help():
-    print("\t _______  _______  __   __  _______  _______")
-    print("\t|       ||       ||  | |  ||  _    ||       |")
-    print("\t|   _   ||  _____||  | |  || |_|   ||    ___|")
-    print("\t|  | |  || |_____ |  |_|  ||       ||   |___")
-    print("\t|  |_|  ||_____  ||       ||  _   | |    ___|")
-    print("\t|       | _____| ||       || |_|   ||   |___")
-    print("\t|_______||_______||_______||_______||_______|")
-    print()
-    print()
-    print("\t [ Extract multiple osu!'s beatmap (.osz) ]")
-    print()
-    print("\t __________________________________________")
-    print("\t|                                          |")
-    print("\t|            Extract Osu! Beatmap          |")
-    print("\t|__________________________________________|")
-    print("\t|                                          |")
-    print("\t|        osube [ extract directory ]       |")
-    print("\t|       osube /home/username/osu/Songs     |")
-    print("\t|__________________________________________|")
-    print("\t|                                          |")
-    print("\t| Run this command in the folder that have |")
-    print("\t|         .osz file to extract.            |")
-    print("\t|__________________________________________|")
-    print()
-    print()
-    print("\t __________________________________________")
-    print("\t|                                          |")
-    print("\t|            Keep Osu! Beatmap             |")
-    print("\t|__________________________________________|")
-    print("\t|                                          |")
-    print("\t|      osube [ extract directory ] -k      |")
-    print("\t|     osube [ extract directory ] --keep   |")
-    print("\t|__________________________________________|")
-    print("\t|                                          |")
-    print("\t|  By default, osube will remove beatmaps  |")
-    print("\t|             after extracted.             |")
-    print("\t|__________________________________________|")
-    print("\t|                                          |")
-    print("\t|      Add [-k, --keep] flag to keep       |")
-    print("\t|        beatmaps after extracted.         |")
-    print("\t|__________________________________________|")
-    print()
-    print()
-
+def is_name_valid(name):
+    return re.match(BEATMAP_PATTERN, name, re.M | re.I | re.U)
 
 def run():
+    parser = argparse.ArgumentParser(prog="osube", description="Extract osu! beatmap. (.osz)")
+    parser.add_argument("File", nargs="*", help="Beatmap files")
+    parser.add_argument("Path", type=str, help="Extract path")
+    parser.add_argument("-k", "--keep", action="store_true", help="Keep beatmap after extract")
+    parser.add_argument("-s", "--silent", action="store_true", help="Suppress result")
 
-    if len(sys.argv) > 1:
+    args = parser.parse_args();
+    extract_dir = args.Path
 
-        if ("-h" in sys.argv) or ("--help" in sys.argv):
-            help()
+    if not os.path.isdir(extract_dir):
+        print("Extract path does not exist")
+        return
 
+    #Retreive .osz file
+    beatmaps = args.File
+
+    if len(beatmaps) <= 0:
+        for item in os.listdir():
+            if is_name_valid(item):
+                beatmaps.append(item)
+
+    #Extract all .osz file to specify directory
+    success_files = []
+    fail_files = []
+
+    for item in beatmaps:
+        if zipfile.is_zipfile(item):
+            with ZipFile(item) as file:
+                folder_name = re.sub(r".osz", "", item)
+                file.extractall(extract_dir + "/" + folder_name)
+                success_files.append(item)
         else:
-            extract_dir = sys.argv[1]
-            current_dir = os.getcwd()
-            current_dir_items = os.listdir(current_dir)
-            beatmap_pattern = "(\w.+osz)"
-            beatmaps = []
-            success_list = []
-            fail_list = []
-            is_keep_beatmaps = False
+            fail_files.append(item)
 
+    #Try to remove beatmaps
+    if not args.keep:
+        for item in beatmaps:
+            os.remove(item)
 
-            #Retreive .osz file in current directory
-            print("Retreive .osz file...")
+    #Show result
+    if args.silent:
+        return
 
-            for item in current_dir_items:
-                if re.match(beatmap_pattern, item, re.M | re.I | re.U):
-                    beatmaps.append(item)
-
-
-            #Extract all .osz file to specify directory
-            print("Extracting...")
-
-            for item in beatmaps:
-                if zipfile.is_zipfile(item):
-                    with ZipFile(item) as beatmap:
-                        beatmap_extract_folder_name = re.sub(r".osz", "", item)
-                        beatmap.extractall(extract_dir + "/" + beatmap_extract_folder_name)
-                        success_list.append(item)
-                else:
-                    fail_list.append(item)
-
-
-            is_keep_beatmaps = ("-k" in sys.argv) or ("--keep" in sys.argv)
-
-            #Remove the beatmaps archive if user doesn't trigger keep option. (-k), (--keep)
-            if not is_keep_beatmaps:
-                for item in beatmaps:
-                    os.remove(item)
-
-
-            #Show extract result
-            print()
-            print()
-            print("\t ----------------------------------------")
-            print("\t|             Extract Result             |")
-            print("\t ----------------------------------------")
-            print()
-            print("\tFail : ", len(fail_list))
-            print("\tSuccess : ", len(success_list))
-            print()
-            print("\tKeep beatmaps : ", is_keep_beatmaps)
-            print()
-            print("\tFrom : ", current_dir)
-            print("\tTo : ", extract_dir)
-            print()
-
-
-            #List the beatmap that successful extracted
-            if (len(success_list) > 0):
-                print("\tSuccessful extract beatmaps : ")
-                print()
-                for item in success_list:
-                    print("\t\t- ", item)
-                print()
-
-
-            #List the beatmap that unsuccessful extracted
-            if (len(fail_list) > 0):
-                print("\tFail to extract beatmaps : ")
-                print()
-                for item in fail_list:
-                    print("\t\t- ", item)
-                print()
-                print()
-
-
-            #Prevent terminal from immediatly close
-            print()
-            print("Enjoy! your beatmaps \t:)")
-            input("Press enter to quit ")
-            print()
-            print()
-
-    else:
+    if (len(success_files) > 0):
+        print("Successful extract beatmaps : ")
+        print("------------------------------")
+        for item in success_files:
+            print(item)
         print()
-        print("Please specify extract directory.\t\t:(")
+
+    if (len(fail_files) > 0):
+        print("Fail to extract beatmaps : ")
+        print("------------------------------")
+        for item in fail_files:
+            print(item)
         print()
-        help()
 
 
 if __name__ == "__main__":
     run()
+
